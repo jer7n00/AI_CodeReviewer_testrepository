787c2e3150823a9a9e2bc39f4eb447e0a7d222da
jer7n00 <166130937+jer7n00@users.noreply.github.com>
2025-07-08 17:14:41 +0530

    Update BuggyCode.java

--- BuggyCode.java
+++ BuggyCode.java
@@ -1,6 +1,6 @@
 public class BuggyCode {
     public static void main(String[] args) {
-        int a = 9;
+        int a = 10;
         int b = 0;
         System.out.println(a / b); // Divide by zero error
     }
